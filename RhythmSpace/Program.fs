b666ccc57581545fcb4c9562fb2d2364c9274f56
Harold <hhausman@gmail.com>
2013-02-11 21:43:26 -0700

    bit of a gui, and some data

--- RhythmSpace/Program.fs
+++ RhythmSpace/Program.fs
@@ -1,43 +1,90 @@
-﻿let outputDevice = Seq.find (fun (device:Midi.OutputDevice) -> device.Name.Contains("LoopBe")) Midi.OutputDevice.InstalledDevices
+﻿open System.Windows.Forms
+open System.Drawing
+
+type Strip() =
+    let numerator = 0
+    let denominator = 1
+    let data = new System.Collections.BitArray(16)
+    member this.isOn(i) = data.[i]
+    member this.set(i,v) = data.Set(i,v)
+
+let strips = Array.init 4 (fun i -> new Strip())
+strips.[0].set(0, true)
+strips.[0].set(10, true)
+
+strips.[1].set(4, true)
+strips.[1].set(12, true)
+
+strips.[2].set(2, true)
+strips.[2].set(6, true)
+strips.[2].set(8, true)
+strips.[2].set(14, true)
+
+let outputDevice = Seq.find (fun (device:Midi.OutputDevice) -> device.Name.Contains("LoopBe")) Midi.OutputDevice.InstalledDevices
 outputDevice.Open()
 let clock = new Midi.Clock(130.f)
 
 let list = ref (new System.Collections.Generic.List<Midi.Message>())
 
+let note pitch time =
+    (!list).Add(new Midi.NoteOnMessage(outputDevice,Midi.Channel.Channel1,pitch,127,time))
+    (!list).Add(new Midi.NoteOffMessage(outputDevice,Midi.Channel.Channel1,pitch,127,time+1.f))
+
 let callbackHandler time =
     let t = time + 4.f
+    (!list).RemoveRange(1,(!list).Count-1)
+    for y = 0 to 3 do
+        let p = match y with
+                | 0 -> Midi.Pitch.C3
+                | 1 -> Midi.Pitch.D3
+                | 2 -> Midi.Pitch.E3
+                | 3 -> Midi.Pitch.F3
+                | _ -> Midi.Pitch.C3
+        for x = 0 to 15 do
+            if strips.[y].isOn(x) then note p (0.25f * (float32 x))
     clock.Schedule( !list, t )
 
 let deligate = (new Midi.CallbackMessage.CallbackType(callbackHandler))
 
 let callback = new Midi.CallbackMessage( deligate, 0.f )
 
-let note pitch time =
-    (!list).Add(new Midi.NoteOnMessage(outputDevice,Midi.Channel.Channel1,pitch,127,time))
-    (!list).Add(new Midi.NoteOffMessage(outputDevice,Midi.Channel.Channel1,pitch,127,time+1.f))
-
 list := (new System.Collections.Generic.List<Midi.Message>())
 (!list).Add( callback )
-note Midi.Pitch.C3 0.f
-note Midi.Pitch.C3 1.f
-note Midi.Pitch.C3 2.f
-note Midi.Pitch.C3 3.f
-note Midi.Pitch.D3 1.f
-note Midi.Pitch.D3 3.f
-note Midi.Pitch.E3 0.5f
-note Midi.Pitch.E3 1.5f
-note Midi.Pitch.E3 2.5f
-note Midi.Pitch.E3 3.5f
 clock.Schedule(!list,0.f)
+
+type MyForm() as self =
+  inherit Form()
+  do
+    self.SetStyle(ControlStyles.DoubleBuffer ||| ControlStyles.UserPaint ||| ControlStyles.AllPaintingInWmPaint, true)
+    self.UpdateStyles()
+    self.FormBorderStyle <- FormBorderStyle.Fixed3D
+    self.ClientSize <- new Size(32*16,32*4)
+
+let f = new MyForm(Text="Rhythm Space")
+
+let rendersquare (g:Graphics) x y value =
+    let rowcolor = match y with
+                   | 0 -> Color.DarkCyan
+                   | 1 -> Color.DarkMagenta
+                   | 2 -> Color.DarkGoldenrod
+                   | _ -> Color.White
+    let c = if value then rowcolor else Color.FromArgb(32,32,32)
+    g.FillRectangle(new SolidBrush(Color.FromArgb(48,48,48)),x*32,y*32,32,32)
+    g.FillRectangle(new SolidBrush(c),x*32+1,y*32+1,32-1,32-1)
+
+let redraw (args:PaintEventArgs) =
+    let g = args.Graphics
+    for y = 0 to 3 do
+        for x = 0 to 15 do
+            rendersquare g x y (strips.[y].isOn(x))
+
+f.Paint.Add(redraw)
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
     clock.Start()
-    let quit = ref false
-    while not !quit do
-        let s = System.Console.ReadLine()
-        if s="q" then quit := true
+    Application.Run(f)
     clock.Stop()
     outputDevice.Close()
     0 // return an integer exit code
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 There is a space of possible rhythms.
 
 But what's where? And, is there any *symmetry*?
+
+![ss](ss.png)
